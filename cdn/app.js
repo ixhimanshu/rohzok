c0cbe169d4d4c750292591ae9f1fd13856861d29
ixhimanshu <40325635+ixhimanshu@users.noreply.github.com>
2019-06-28 00:59:54 +0530

    Update app.js

--- cdn/app.js
+++ cdn/app.js
@@ -1,13 +1,5 @@
  
-// var current_user;
 
-// function sendusername(event){
-//   event.preventDefault();
-//   console.log(event.target.username.value)
-//   var current_user = event.target.username.value;
-//   fetchnow(current_user)
-//   return current_user;
-// }
 
 
 var client_username = document.getElementById("show").innerText;
@@ -15,7 +7,6 @@ var client_username = document.getElementById("show").innerText;
 var client_abc = `https://www.instagram.com/${client_username}/?__a=1`;
 
 
-// document.getElementById("show").innerText = "";
 
 
  
@@ -25,15 +16,12 @@ var client_abc = `https://www.instagram.com/${client_username}/?__a=1`;
   .then( (res) => res.json() )
   .then( (data) => {
    
-  //   console.log(data.graphql.user.edge_owner_to_timeline_media.edges[0].node.display_url)
+ 
     
   for(i=0; i<10; i++) {
 
     photo = data.graphql.user.edge_owner_to_timeline_media.edges[i].node.display_url;
 
-
-  //   console.log(data.graphql.user.edge_owner_to_timeline_media)
-  //   console.log(data.graphql)
 
   var node = document.createElement("img");
   node.setAttribute('src', `${photo}`);
@@ -50,9 +38,9 @@ var client_abc = `https://www.instagram.com/${client_username}/?__a=1`;
 
   } )
 
-  setTimeout(() => {
-    document.getElementById('code').style.display = "block";
-  }, 2000)
+//   setTimeout(() => {
+//     document.getElementById('code').style.display = "block";
+//   }, 2000)
  
 
  }
